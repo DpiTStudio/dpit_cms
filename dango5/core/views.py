dea4f897dc7f74115660e1c9df8e737a26927434
DpiTStudio <admin@dpitstudio.ru>
2025-04-16 01:20:31 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   dango5/core/__pycache__/views.cpython-311.pyc
    	new file:   dango5/core/templates/news/list.html
    	new file:   dango5/core/templates/portfolio/list.html
    	modified:   dango5/core/views.py

--- dango5/core/views.py
+++ dango5/core/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import News, Portfolio, Page
+from django.core.paginator import Paginator
+
+def portfolio_list(request):
+    portfolio_list = Portfolio.objects.filter(is_published=True).order_by('-created_date')
+    paginator = Paginator(portfolio_list, 6)  # Показывать 6 работ на странице
+    page_number = request.GET.get('page')
+    portfolio = paginator.get_page(page_number)
+    return render(request, 'portfolio/list.html', {'portfolio': portfolio})
+
+def news_list(request):
+    news_list = News.objects.filter(is_published=True).order_by('-published_date')
+    paginator = Paginator(news_list, 6)  # Показывать 6 новостей на странице
+    page_number = request.GET.get('page')
+    news = paginator.get_page(page_number)
+    return render(request, 'news/list.html', {'news': news})
 
 def index(request):
     news = News.objects.filter(is_published=True).order_by('-published_date')[:3]
